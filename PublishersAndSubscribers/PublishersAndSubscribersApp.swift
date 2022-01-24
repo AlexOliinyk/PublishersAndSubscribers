decf31500ded41af29f326dc4e86917174a3c12a
AlexOliinyk <alex.oliinyk@gmail.com>
2022-01-24 10:57:50 +0200

    Initial Commit

---
+++ PublishersAndSubscribers/PublishersAndSubscribersApp.swift
@@ -0,0 +1,17 @@
+//
+//  PublishersAndSubscribersApp.swift
+//  PublishersAndSubscribers
+//
+//  Created by Oleksandr Oliinyk on 24.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct PublishersAndSubscribersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
